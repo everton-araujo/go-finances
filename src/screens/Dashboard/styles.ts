187beff8fec570414af288bf31c532b2cbbb417c
Everton <everton.aaraujo@outlook.com>
2022-02-22 07:45:08 -0300

    Global style and custom fonts added

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,8 +4,11 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
-
+  font-family: ${({ theme }) => theme.fonts.bold};
+  color: ${({ theme }) => theme.colors.title};
 `;
